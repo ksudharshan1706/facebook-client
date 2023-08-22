d5b55298c393bd3cbb8521ab2a92f1ee906a41b0
ksudharshan1706 <kamavaramsudharshan1706@gmail.com>
2023-08-22 13:54:06 +0530

    facebook Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Profile from "./pages/profile/Profile";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import "./index.css";
+import Search from "./components/search/Search.js";
+import { AuthContext } from "./context/Authcontext";
+import Messenger from "./pages/messenger/Messenger";
+import ProfileFriend from "./pages/profile/ProfileFriend";
 function App() {
+  const { user } = useContext(AuthContext);
+  // console.log(user);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={user ? <Home /> : <Register />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate replace tos="/" /> : <Register />}
+        />
+        <Route
+          path="/login"
+          element={user ? <Navigate replace to="/" /> : <Login />}
+        />
+        <Route
+          path="/messenger"
+          element={user ? <Messenger /> : <Navigate replace to="/" />}
+        />
+        <Route path="/profile/:username" element={<Profile />} />
+        <Route path="/profilefriend/:userId" element={<ProfileFriend />} />
+        <Route path="/searchpage" element={<Search />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+import { AuthContextProvider } from "./context/Authcontext.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+axios.defaults.baseURL = "http://localhost:8800/api/";
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </React.StrictMode>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
