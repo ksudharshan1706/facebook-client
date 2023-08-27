01acaf9b630433f1d7cffb8e7f6985980c97741a
ksudharshan1706 <kamavaramsudharshan1706@gmail.com>
2023-08-27 14:30:16 +0530

    freinds

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -64,6 +64,7 @@ export default function Sidebar() {
         {/* <button className="sidebarButton">Show More</button> */}
         <hr className="sidebarHr" />
         <ul className="sidebarFriendList">
+        <h4 className="sidebarTitle">Friends</h4>
           {Users?Users.map((u)=>(<CloseFriend key={u.id} user={u}/>)):null}
         </ul>
       </div>
